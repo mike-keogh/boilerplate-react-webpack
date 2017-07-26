c23bee8fe1f9be02b21feab18a23058bb0d85b8a
Michael Keogh <brookemossburn@gmail.com>
2017-07-27 11:32:53 +1200

    Seeded player data

---
+++ data/team.js
@@ -0,0 +1,70 @@
+const team = {
+  players: [
+    {
+      id: 1,
+      name: 'Felix Faisandier',
+      characteristic: 'Right hand bat, right arm beamers',
+      nickname: 'fe fi fo'
+    },
+    {
+      id: 2,
+      name: 'Jason Foote',
+      characteristic: 'Right hand everything',
+      nickname: 'give em a jase'
+    },
+    {
+      id: 3,
+      name: 'Jack Foster',
+      characteristic: 'Right hand bat, right arm dibbly dobblys',
+      nickname: 'post-structuralism'
+    },
+    {
+      id: 4,
+      name: 'Evan Frost',
+      characteristic: 'Right arm bat, left hand top of off stump',
+      nickname: 'kev, ev, van man, vo'
+    },
+    {
+      id: 5,
+      name: 'Gabriel George-Baker',
+      characteristic: 'Front foot fetish, right arm aggression',
+      nickname: 'dizzy'
+    },
+    {
+      id: 6,
+      name: 'Nick Honey',
+      characteristic: 'Right hand bat, right arm celebrations',
+      nickname: 'honey badger'
+    },
+    {
+      id: 7,
+      name: 'Mike Keogh',
+      characteristic: 'Left hand bat, right arm bruiser',
+      nickname: 'kony'
+    },
+    {
+      id: 8,
+      name: 'Paul Matthews',
+      characteristic: 'Right hand bat, right arm outta nowhere',
+      nickname: 'Steven Speilberg'
+    },
+    {
+      id: 9
+      name: 'Joe Sloane',
+      characteristic: 'Right hand bat, right arm bouncers',
+      nickname: 'frosty'
+    },
+    {
+      id: 10
+      name: 'Rhys Stannard',
+      characteristic: 'Right arm bat, right arm revenge',
+      nickname: 'cap, moggy, ol nine lives'
+    },
+    {
+      id: 11,
+      name: 'Isaiah Wilson',
+      characteristic: 'Right arm bat, right arm you gettin a bouncer',
+      nickname: 'mf zazu'
+    }
+  ]
+}
